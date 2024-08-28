9628529ae3df7336a70f1bfe2fe1db0aa23a3671
Aymeric DEVRIESERE <aymeric.devriesere@cgi.com>
2024-08-28 14:57:09 +0000

    Update pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { withAuthProvider } from "./middlewares/withAuthorization";
 import { withAuth } from "next-auth/middleware";
-export default withAuthProvider("keycloak");
-
-export default withAuth({
-  pages: {
-    signIn: "/auth/signin",
-  },
-});
+export default withAuthProvider("keycloak")
 
 export const config = {
   matcher: ["/"],
