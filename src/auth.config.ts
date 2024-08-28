8578166df7ef7edafc288bef53e9d765495d38f5
Aymeric DEVRIESERE <aymeric.devriesere@cgi.com>
2024-08-28 15:47:35 +0000

    Update pages

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,9 +9,6 @@ export const authOptions: NextAuthOptions = {
       issuer: process.env.KEYCLOAK_ISSUER,
     }),
   ],
-  pages: {
-    signIn: process.env.AUTH_PAGE || "/api/auth/signin",
-  },
   callbacks: {
     jwt({ token, account, user }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
