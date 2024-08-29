7b4421e891b08057f1862a7fdf964f3ce1d326d9
Aymeric DEVRIESERE <aymeric.devriesere@cgi.com>
2024-08-29 07:55:15 +0000

    Update middleware matchers

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,5 +2,5 @@ import { withAuthProvider } from "./middlewares/withAuthorization";
 export default withAuthProvider("keycloak")
 
 export const config = {
-  matcher: ["/", "/(.*)"],
+  matcher: ["/"],
 };
