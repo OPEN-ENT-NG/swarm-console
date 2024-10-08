5d139fb3ec9aad50813b81f5f0455db4714e0903
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-10-08 18:35:28 +0200

    wip

--- src/containers/ToggleStatusServicesModal/utils.ts
+++ src/containers/ToggleStatusServicesModal/utils.ts
@@ -1,4 +1,5 @@
 import { SERVICE_STATE, SERVICE_TYPE } from "@/providers/GlobalProvider/enums";
+import { Service } from "@/providers/GlobalProvider/serviceType";
 import { RowItem } from "@/providers/GlobalProvider/types";
 import { ToggleStatusBody } from "@/services/types";
 
@@ -12,9 +13,16 @@ export const createToggleStatusBody = (
   isActivating: boolean,
 ): ToggleStatusBody => {
   const services = tableSelected.flatMap(row => row.services);
-  const servicesToToggle = services.filter(service => inputValue.includes(service.type));
+
+  const servicesToToggle = services.filter(
+    service => inputValue.includes(service.type) && shouldToggleService(service, isActivating),
+  );
 
   const newState = isActivating ? SERVICE_STATE.REACTIVATION_SCHEDULED : SERVICE_STATE.DEACTIVATION_SCHEDULED;
+
+  if (servicesToToggle.length === 0) {
+    return [];
+  }
 
   return [
     {
@@ -22,4 +30,11 @@ export const createToggleStatusBody = (
       state: newState,
     },
   ];
+};
+
+const shouldToggleService = (service: Service, isActivating: boolean): boolean => {
+  if (isActivating) {
+    return service.state === SERVICE_STATE.DISABLED;
+  }
+  return service.state === SERVICE_STATE.DEPLOYED;
 };
